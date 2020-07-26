a4c2be7e1f6726808d3b6b15bc3022cb32c4a1b2
Moshe Gelberg <glbrgm@gmail.com>
2020-07-27 01:37:53 +0300

    Added Register, Login and JWT

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -3,12 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext: DbContext
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
